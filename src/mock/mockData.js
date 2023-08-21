d082d1db01cf45f51a2634644f4e11cae56c8867
leoHcodar <suporte@horadecodar.com.br>
2023-08-21 00:36:39 -0300

    feat: create mockdata

---
+++ src/mock/mockData.js
@@ -0,0 +1,135 @@
+// mockData.js
+
+export const questions = [
+    {
+        question: 'O que mais define você ?',
+        choices: ['a) Determinado', 'b) Confiante', 'c) Consistente', 'd) Preciso'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Apressado', 'b) Persuasivo', 'c) Metódico', 'd) Cuidadoso'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Competitivo', 'b) Político', 'c) Cooperativo', 'd) Diplomata'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Objetivo', 'b) Exagerado', 'c) Estável', 'd) Exato'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Assertivo', 'b) Otimista', 'c) Paciente', 'd) Prudente'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Fazedor', 'b) Inspirador', 'c) Persistente', 'd) Perfeccionista'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Agressivo', 'b) Expansivo', 'c) Possessivo', 'd) Julgador'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Decidido', 'b) Flexível', 'c) Previsível', 'd) Sistemático'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Decidido', 'b) Flexível', 'c) Previsível', 'd) Sistemático'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Inovador', 'b) Comunicativo', 'c) Agradável', 'd) Elegante'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Autoritário', 'b) Extravagante', 'c) Modesto', 'd) Dependente'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Enérgico', 'b) Entusiasmado', 'c) Calmo', 'd) Disciplinado'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Firme', 'b) Expressivo', 'c) Amável', 'd) Formal'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Obstinado', 'b) Criativo', 'c) Ponderado', 'd) Detalhista'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Egocêntrico', 'b) Tagarela', 'c) Acomodado', 'd) Refém'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Inspira Confiança', 'b) Convincente', 'c) Compreensivo', 'd) Pontual'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Intimidade', 'b) Indiscreto', 'c) Reservado', 'd) Intransigente'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Vigoroso', 'b) Caloroso', 'c) Gentil', 'd) Atento'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Ousado', 'b) Sedutor', 'c) Harmonizador', 'd) Cauteloso'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Força De Vontade', 'b) Espontâneo', 'c) Satisfeito', 'd) Conservador'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Exigente', 'b) Sociável', 'c) Leal', 'd) Rigoroso'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Pioneiro', 'b) Divertido', 'c) Tranquilo', 'd) Convencional'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Ambicioso', 'b) Chamativo', 'c) Regulado', 'd) Calculista'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Inquisitivo', 'b) Dado', 'c) Ingênuo', 'd) Cético'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Audacioso', 'b) Extrovertido', 'c) Casual', 'd) Meticuloso'],
+        selectedLetter: ''
+      },
+      {
+        question: 'O que mais define você ?',
+        choices: ['a) Direto', 'b) Jovial', 'c) Moderado', 'd) Processual'],
+        selectedLetter: ''
+      }
+  ];
+  
